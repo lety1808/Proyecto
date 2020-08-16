f73802c4c9fa1cc31286569c16ef9f13d55cf153
Leticia Goycoechea <68928243+lety1808@users.noreply.github.com>
2020-08-16 19:07:14 -0300

    f

--- js/login.js
+++ js/login.js
@@ -6,8 +6,8 @@
         var usuario = document.getElementById("Usuario").value;
         var contrasena = document.getElementById("Contraseña").value;
         if(Usuario!="" && Contraseña!"="")
-        //document.form,submit();//
-        windo.location="principal.html";
+        //document.form.submit();//
+        window.location="principal.html";
         auxi = 1;
     } else {
         alert("Por favor ingrese nombre de usuario y contraseña correctos")
