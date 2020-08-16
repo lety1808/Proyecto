32c224e02b1df442d30d63b706a05191dab5bf34
Leticia Goycoechea <68928243+lety1808@users.noreply.github.com>
2020-08-16 18:58:56 -0300

    modif

--- js/login.js
+++ js/login.js
@@ -1,6 +1,17 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+    function login(){
+        var usuario = document.getElementById("Usuario").value;
+        var contrasena = document.getElementById("Contraseña").value;
+        if(Usuario!="" && Contraseña!"="")
+        //document.form,submit();//
+        windo.location="principal.html";
+        auxi = 1;
+    } else {
+        alert("Por favor ingrese nombre de usuario y contraseña correctos")
+    }
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
